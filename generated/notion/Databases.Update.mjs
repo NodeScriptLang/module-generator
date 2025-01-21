a39a08ac16de2a8c867088db3f02412db6ee51d2
christellevs <christelle.marie@gmail.com>
2025-01-21 18:36:54 +0000

    chore: add missing notion .msj files

--- generated/notion/Databases.Update.mjs
+++ generated/notion/Databases.Update.mjs
@@ -3,10 +3,14 @@ export async function compute(params, ctx) {
     "https://api.notion.com/v1/databases/{id}"
       .replace("{id}", params["id"])
   );
-  const addQueryParam = (key, val) => { if (val !== undefined) url.searchParams.append(key, val) };
   const headers = {};
-  headers["Authorization"] = ("Bearer" + " " + params["auth"].replace(/^Bearer\s+/gi, ''));
-  headers["Notion-Version"] = params["notionVersion"];
+  const addQueryParam = (key, val) => { if (val != null) url.searchParams.append(key, val) };
+  if (params["auth"] != null) {
+    headers["Authorization"] = ("Bearer" + " " + params["auth"].replace(/^Bearer\s*/gi, ''));
+  }
+  if (params["notionVersion"] != null) {
+    headers["Notion-Version"] = params["notionVersion"];
+  }
   headers["content-type"] = "application/json";
   let body = {};
   body["properties"] = params["properties"];
